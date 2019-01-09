f4b145f8b2fc5dd813df413c108120ad2872fc83
sxia1 <sxia1@stuy.edu>
2019-01-09 12:30:52 -0500

    add search functions

--- util/db_search.py
+++ util/db_search.py
@@ -29,6 +29,61 @@ def username(username):
     userList = c.fetchall()
     return userList
 
+'''
+getAvatar(username)
+returns an empty list if username doesn't exist in the database
+returns [avatar] if username exists
+'''
+def getAvatar(username):
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+    getAva = 'SELECT avatar FROM users WHERE users.username = (?);'
+    c.execute(getAva,(username,))
+    ava = c.fetchall()
+    return ava
+
+'''
+getDifficulty(username)
+returns a an empty list if the user doesn't exist or if the user hasn't inputted any tasks.
+returns a list of all the difficulties
+'''
+def getDifficulty():
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+    listDiff = 'SELECT difficulty FROM todo WHERE todo.username = (?);'
+    c.execute(listDiff,(username,))
+    diffList = c.fetchall()
+    return diffList
+
+'''
+getTask(username)
+returns a an empty list if the user doesn't exist or if the user hasn't inputted any tasks.
+returns a list of all the tasks
+'''
+def getTask():
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+    listTasks = 'SELECT task FROM todo WHERE todo.username = (?);'
+    c.execute(listTasks,(username,))
+    taskList = c.fetchall()
+    return taskList
+
+'''
+getPokemon(username)
+returns a an empty list if the user doesn't exist or if the user hasn't obtained any pokemon
+returns a list of all the pokemon
+'''
+def getPokemon(username):
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+    listPoke = 'SELECT cardID FROM pokemon WHERE pokemon.username = (?);'
+    c.execute(listPoke,(username,))
+    pokeList = c.fetchall()
+    return pokeList
     
     
     
