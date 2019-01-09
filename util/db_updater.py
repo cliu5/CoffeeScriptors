f3864e32779afaa66df9f654c7da5b4149a52258
ecrystale <elee19@stuy.edu>
2019-01-09 12:31:38 -0500

    update dbs

--- util/db_updater.py
+++ util/db_updater.py
@@ -9,12 +9,32 @@ username is the username of the user
 password is the password of the user
 function adds the username and password to the users database
 '''
-def adduser(username, password):
+def adduser(username, password, pokemon,ID):
     DB_FILE="data/CoffeeScriptors.db"
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    insert = "INSERT INTO users VALUES(?,?,?)"
-    params=(username,password,0)
+    insert = "INSERT INTO users VALUES(?,?,?,?,?)"
+    params=(username,password,0,pokemon,ID)
+    c.execute(insert,params)
+    db.commit()
+    db.close()
+
+def addtask(username, difficulty, task): #adds a task to the todo list
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    insert = "INSERT INTO todo VALUES(?,?,?)"
+    params=(username, difficulty, task)
+    c.execute(insert,params)
+    db.commit()
+    db.close()
+
+def addpokemon(username, cardID):
+    DB_FILE="data/CoffeeScriptors.db"
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    insert = "INSERT INTO pokemon VALUES(?,?)"
+    params=(username,cardID)
     c.execute(insert,params)
     db.commit()
     db.close()
