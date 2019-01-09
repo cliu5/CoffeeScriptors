3af6ec227e2314570b5d77c856ffced86915a61c
ecrystale <elee19@stuy.edu>
2019-01-09 12:19:39 -0500

    creating the tables

--- util/db_builder.py
+++ util/db_builder.py
@@ -1,17 +1,18 @@
 import sqlite3 #imports sqlite
 
 def users(): #creates the users db
-    DB_FILE="./data/CoffeeScriptors.db" 
+    DB_FILE="./data/CoffeeScriptors.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
-    command = "CREATE TABLE users(username TEXT, password TEXT, score INTEGER)"
+    command = "CREATE TABLE users(username TEXT, password TEXT, gold INTEGER, INTEGER PRIMARY KEY (ID))" #user table
     c.execute(command)
-
+    command = "CREATE TABLE todo(username TEXT, difficulty INTEGER, task TEXT)" #to-do list table
+    c.execute(command)
+    command = "CREATE TABLE pokemon(username TEXT, task TEXT)" #pokemon table
+    c.execute(command)
 
 def main(): #calls all of the functions to build the databases
     try:
-        
         users()
-        questions()
     except:
         pass
