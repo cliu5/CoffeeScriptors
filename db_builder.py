a018e9711a23549a813ee2ed9b5a95ea07f0e3d3
cliu5 <cliu5@stuy.edu>
2019-01-09 12:02:31 -0500

    added database code, login/register code, templates for html

---
+++ db_builder.py
@@ -0,0 +1,17 @@
+import sqlite3 #imports sqlite
+
+def users(): #creates the users db
+    DB_FILE="./data/CoffeeScriptors.db" 
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor() #facilitates db operations
+    command = "CREATE TABLE users(username TEXT, password TEXT, score INTEGER)"
+    c.execute(command)
+
+
+def main(): #calls all of the functions to build the databases
+    try:
+        
+        users()
+        questions()
+    except:
+        pass
