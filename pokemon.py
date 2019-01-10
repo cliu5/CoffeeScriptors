2f549edf3e1ec9bdb0f329019cec5fd2faefbe4d
StefanTan18 <stan3@stuy.edu>
2019-01-10 12:34:49 -0500

    added pokemon

--- pokemon.py
+++ pokemon.py
@@ -5,10 +5,18 @@ from urllib.request import Request
 URL_STUB = "https://api.pokemontcg.io/v1/cards/base1-"
 
 '''
+Starter Pokemon
+Squirtle: 63
+Charmander: 46
+Bulbasaur: 44
+'''
+STARTERS = [44, 46, 63] 
+
+'''
 Gets json data from Pokemon TCG API.
 Retrieves the name of all pokemons from the set.
 '''
-def getPokemon():
+def getAllPokemon():
     pokeList = []
     for i in range(1, 70):
         URL = URL_STUB + str(i)
@@ -22,6 +30,68 @@ def getPokemon():
         pokeList.append(data["card"]["name"])
 
     return pokeList
+
+'''
+Retrieves the starter pokemons.
+'''
+def getStarters():
+    starterList = []
+
+    for id in STARTERS:
+        URL = URL_STUB + str(id)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
+    
+        return data["card"]["name"]
+
+
+'''
+Retrieves the hp of the inputed pokemon.
+'''
+def getHP(pokemon):
+    for i in range(1, 70):
+        URL = URL_STUB + str(i)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
+
+        if (pokemon == data["card"]["name"]):
+            return data["card"]["hp"]
+
+
+'''
+Retrieves the pokemon based on the ID.
+'''
+def getPokemon(id):
+    URL = URL_STUB + str(id)
+    URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+    response = request.urlopen(URL)
+    response = response.read()
+    data = json.loads(response)
+
+    return data["card"]["name"] 
+
+'''
+Retrieves the id of the inputed pokemon.
+'''
+def getID(pokemon):
+    for i in range(1, 70):
+        URL = URL_STUB + str(i)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
+
+        if (pokemon == data["card"]["name"]):
+            return data["card"]["id"]
+
 
 '''
 Retrieves the hp of the inputed pokemon.
@@ -54,6 +124,8 @@ def getImage(pokemon):
             return data["card"]["imageUrl"]
 
         
-print(getPokemon())
+#print(getAllPokemon())
+print(getStarters())
+print(getID("Alakazam"))
 print(getHP("Alakazam"))
 print(getImage("Alakazam"))
