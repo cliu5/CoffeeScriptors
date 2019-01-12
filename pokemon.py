05281ceb7e0e1ae793e97ae4123924a21d6646a1
Stefan Tan <33496362+StefanTan18@users.noreply.github.com>
2019-01-12 18:28:39 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -37,16 +37,18 @@ Retrieves the starter pokemons.
 def getStarters():
     starterList = []
 
-    for id in STARTERS:
-        URL = URL_STUB + str(id)
+    for i in STARTERS:
+        #print(i)
+        URL = URL_STUB + str(i)
         URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
         
         response = request.urlopen(URL)
         response = response.read()
         data = json.loads(response)
-    
-        return data["card"]["name"]
+        
+        starterList.append(data["card"]["name"])
 
+    return starterList
 
 '''
 Retrieves the hp of the inputed pokemon.
@@ -63,7 +65,56 @@ def getHP(pokemon):
         if (pokemon == data["card"]["name"]):
             return data["card"]["hp"]
 
+'''
+Retrieves the types of the inputed pokemon.
+'''
+def getType(pokemon):
+    for i in range(1, 70):
+        URL = URL_STUB + str(i)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
 
+        if (pokemon == data["card"]["name"]):
+            return data["card"]["types"]
+
+'''
+Retrieves the hp of the inputed pokemon.
+'''
+def getRarity(pokemon):
+    for i in range(1, 70):
+        URL = URL_STUB + str(i)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
+
+        if (pokemon == data["card"]["name"]):
+            return data["card"]["rarity"]
+
+'''
+Retrieves the attacks of the inputed pokemon in the form of a dictionary.
+'''
+def getAttacks(pokemon):
+    atkDict = {}
+    
+    for i in range(1, 70):
+        URL = URL_STUB + str(i)
+        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        
+        response = request.urlopen(URL)
+        response = response.read()
+        data = json.loads(response)
+
+        if (pokemon == data["card"]["name"]):
+            for j in data["card"]["attacks"]:
+                attkDict[data["card"]["attacks"]["name"]] = data["card"]["attacks"]["damage"]
+
+    return atkDict
+        
 '''
 Retrieves the pokemon based on the ID.
 '''
@@ -88,25 +139,10 @@ def getID(pokemon):
         response = request.urlopen(URL)
         response = response.read()
         data = json.loads(response)
-
+        
         if (pokemon == data["card"]["name"]):
             return data["card"]["id"]
 
-
-'''
-Retrieves the hp of the inputed pokemon.
-'''
-def getHP(pokemon):
-    for i in range(1, 70):
-        URL = URL_STUB + str(i)
-        URL = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
-        
-        response = request.urlopen(URL)
-        response = response.read()
-        data = json.loads(response)
-
-        if (pokemon == data["card"]["name"]):
-            return data["card"]["hp"]
 
 '''
 Retrieves the image of the inputed pokemon
@@ -121,11 +157,13 @@ def getImage(pokemon):
         data = json.loads(response)
 
         if (pokemon == data["card"]["name"]):
-            return data["card"]["imageUrl"]
+            return data["card"]["imageUrlHiRes"]
 
         
 #print(getAllPokemon())
 print(getStarters())
 print(getID("Alakazam"))
 print(getHP("Alakazam"))
+print(getType("Alakazam"))
+print(getRarity("Alakazam"))
 print(getImage("Alakazam"))
