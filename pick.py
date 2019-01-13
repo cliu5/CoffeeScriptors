487ad286b5f3c559eb93bf78937394bafd4e5eaa
ecrystale <elee19@stuy,edu>
2019-01-13 10:32:10 -0500

    pick works: must choose a type and press submit -> tells which pokemon you get

--- pick.py
+++ pick.py
@@ -1,8 +1,10 @@
 from flask import Flask,render_template,request,session,url_for,redirect,flash
 import random
+from os import urandom
 import urllib
 
 app = Flask(__name__)
+app.secret_key = urandom(32)
 
 @app.route("/",methods=['GET','POST'])
 def ok():
@@ -11,8 +13,14 @@ def ok():
 @app.route("/redone",methods=['GET','POST'])
 def home():
     whattype=request.form['type']
-    flash(whattype)
-    return redirect(url_for("ok"))
+    if (whattype=='grass'):
+        poke="bulbasaur"
+    if (whattype=='fire'):
+        poke="charmander"
+    if (whattype=='water'):
+        poke="squirtle"
+    return render_template("pick2.html",
+                           pokemon=poke)
 
 if __name__ == '__main__':
     app.debug = True
