77492fc9a302fed7f1ee9b8ac58da74db4feb1e6
ecrystale <elee19@stuy,edu>
2019-01-13 10:43:46 -0500

    images for starters

--- pick.py
+++ pick.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,session,url_for,redirect,flash
 import random
 from os import urandom
 import urllib
+import pokemon as pokepy
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
@@ -14,13 +15,17 @@ def ok():
 def home():
     whattype=request.form['type']
     if (whattype=='grass'):
-        poke="bulbasaur"
+        poke="Bulbasaur"
+        img=pokepy.getImage("Bulbasaur")
     if (whattype=='fire'):
-        poke="charmander"
+        poke="Charmander"
+        img=pokepy.getImage("Charmander")
     if (whattype=='water'):
-        poke="squirtle"
+        poke="Squirtle"
+        img=pokepy.getImage("Squirtle")
     return render_template("pick2.html",
-                           pokemon=poke)
+                           pokemon=poke,
+                           pokeimg=img)
 
 if __name__ == '__main__':
     app.debug = True
