680be7101aec8f22292264c27229dec52d27b2f3
ecrystale <elee19@stuy,edu>
2019-01-13 00:28:27 -0500

    working on picking pokemon

---
+++ pick.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request,session,url_for,redirect,flash
+import random
+import urllib
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def ok():
+    return render_template('pick.html')
+
+@app.route("/redone",methods=['GET','POST'])
+def home():
+    whattype=request.form['type']
+    flash(whattype)
+    return redirect(url_for("ok")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
